a026941a60b9a87f8d499e453626d77c8bc902b8
roman-bugaeff <rbugaeff@gmail.com>
2019-08-04 20:34:09 +0300

    added FormsModule

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
-
-  newPost = '';
+  enteredValue = '';
+  newPost = 'NO CONTENT';
 
   onAddPost(){
-    this.newPost = 'The user\'s post';
-  }
+    this.newPost = this.enteredValue;
+    }
 
 }
