50fc2893c2b0fc7417b5f923da69fd2c8aa5f573
roman-bugaeff <rbugaeff@gmail.com>
2019-08-06 05:19:20 +0300

    added post-list

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent {
   enteredValue = '';
-  newPost = 'NO CONTENT';
+  newPost = '';
 
   onAddPost(){
     this.newPost = this.enteredValue;
