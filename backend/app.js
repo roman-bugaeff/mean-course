7c128e3cf66b98522439e29a6b65e162ac437245
roman-bugaeff <rbugaeff@gmail.com>
2019-08-10 17:21:46 +0300

    created array posts in server-side

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'qwer12345',
+      title: 'First server-side post!',
+      content: 'This is coming from the sever'
+    },
+    {
+      id: 'qwer54321',
+      title: 'Second server-side post!',
+      content: 'This is coming from the sever'
+    },
+  ]
+  res.status(200).json({
+    message:'Post fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
