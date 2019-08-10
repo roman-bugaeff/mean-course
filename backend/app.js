0991f489dfe13d778bf07bd84ab2bc0e2c091b1f
roman-bugaeff <rbugaeff@gmail.com>
2019-08-10 09:39:16 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'qwer12345',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'qwer54321',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+    },
+  ];
+  res.status(200).json({
+    message: 'Post fletched succesfully!',
+    posts: posts
+  });
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
-});
+
 
 module.exports = app;
 
